cb8c37426c842a31ab00a64b400df8193648baf3
Ezeoke Godswill <30760648+WillDera@users.noreply.github.com>
2022-06-11 05:44:16 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/doc', app, document);
 
-  await app.listen((process.env.PORT, parseInt('0.0.0.0')) || 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
